930cf170acadb8cc268fa399c9f1315c52c136b6
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-03 13:50:06 -0600

    commit all changes

--- JS-Fundamentals/Array.js
+++ JS-Fundamentals/Array.js
@@ -1,7 +1,3 @@
-let primes = [2, 3, 5, 7, 11];
-if (primes % 1 === 0){
-    console.log("It's  a prime number")
-}
-else {
-    console.log("It's not prime number");
-}
+const num = [2, 3, 5, 7, 11];
+let sortNum = num.sort()
+console.log(sortNum);
