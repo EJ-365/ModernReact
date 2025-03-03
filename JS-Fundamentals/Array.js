bf2e9363560aaad00bcac568d809b8125c84f9ef
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-03 12:30:22 -0600

    update

--- JS-Fundamentals/Array.js
+++ JS-Fundamentals/Array.js
@@ -1,7 +1,7 @@
 let primes = [2, 3, 5, 7, 11];
-if (primes % 2 === 0){
-    console.log("It's not a prime number")
+if (primes % 1 === 0){
+    console.log("It's  a prime number")
 }
 else {
-    console.log("It's a prime number");
+    console.log("It's not prime number");
 }
