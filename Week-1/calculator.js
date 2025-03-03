5b331881477261cf8224222893f1167ab91da07a
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-03 11:42:13 -0600

    updated the file

--- Week-1/calculator.js
+++ Week-1/calculator.js
@@ -1,8 +1,1 @@
-function Info(name, age, occupation){
-    this.name = name;
-    this.age = age;
-    this.occupation = occupation;
-}
-
-const myInfo = new Info('EJ',22, 'Student')
-console.log(myInfo.age);
+console.log("Hello js");
