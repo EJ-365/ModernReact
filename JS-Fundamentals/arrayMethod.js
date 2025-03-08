5a0761d30ee531fefbda5948dad891f06824620f
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-08 01:51:06 -0600

    update file

---
+++ JS-Fundamentals/arrayMethod.js
@@ -0,0 +1,26 @@
+const fruits = ['apple','pineapple','kiwi','strawberry','orange','mango']
+// manipulate it with the array method you know
+
+const num = [1,2,4,6,8];
+let result = num.indexOf(4);
+// console.log(result);  // 2;  search for the index 4 and return the position where 4 is
+
+
+
+/* preview: */
+// function economics(name){
+//     if(name === "Ejeme"){
+//         for (let i = 1; i <= 20; i++){
+//             console.log("Please try and finish the chapter 7 for macro econ");
+//         }
+//     }
+//     else {
+//         console.log('glad you finished it')
+//     }
+// }
+// function display(){
+//     let  myName = 'Ejay Gabriel'
+//     let result = economics(myName);
+// }
+
+// display();
