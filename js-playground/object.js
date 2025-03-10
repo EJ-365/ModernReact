ea1f170cea05b187162178907fc1c66de7952891
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-10 19:04:09 +0000

    Mar 10, 2025, 2:04 PM

--- js-playground/object.js
+++ js-playground/object.js
@@ -1,3 +1,11 @@
 let backPack = {
+    color: 'blue',
+    numOFZip: 5,
+    numOfCupholder: 2,
+    book: {
+        color: 'blue',
+        numOfPages: 80,
+        subject: 'math'
+    },
     
 }
