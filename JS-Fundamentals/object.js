a22615e264c89d6912365c9f188dcd2dc467d328
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-12 13:43:21 -0500

    Updated files

--- JS-Fundamentals/object.js
+++ JS-Fundamentals/object.js
@@ -1,35 +1,36 @@
 let backPack = {
-    color: 'blue',
-    numOFZip: 5,
-    numOfCupholder: 2,
-    book: {
-        color: 'blue',
-        numOfPages: 80,
-        subject: 'calculus'
-    },
-    message: function(){
-        console.log("This is my backpack");
-    },
-    isHeavy: false,
-    containMyLaptop: true,
-    laptop: {
-        color: 'silver',
-        model: 'dell-Inspiron',
-        screenSize: 14.0
-    },
-    display: function(){
-        this.color = "black and blue"
-      console.log(`My backpack is ${this.color} and has ${this.numOFZip} zippers`)
-   console.log(`contains ${this.laptop.model} laptop`)
-   let hasLaptop = this.containMyLaptop 
-   ? "it contains" : "it doesn't contains it"
-   console.log(`This backpack ${hasLaptop} my laptop`)
-}
-}
+  color: "blue",
+  numOFZip: 5,
+  numOfCupholder: 2,
+  book: {
+    color: "blue",
+    numOfPages: 80,
+    subject: "calculus",
+  },
+  message: function () {
+    console.log("This is my backpack");
+  },
+  isHeavy: false,
+  containMyLaptop: true,
+  laptop: {
+    color: "silver",
+    model: "dell-Inspiron",
+    screenSize: 14.0,
+  },
+  display: function () {
+    this.color = "black and blue";
+    console.log(
+      `My backpack is ${this.color} and has ${this.numOFZip} zippers`
+    );
+    console.log(`contains ${this.laptop.model} laptop`);
+    let hasLaptop = this.containMyLaptop ? "contains" : " doesn't contains";
+    console.log(`This backpack ${hasLaptop} my laptop`);
+  },
+};
 
 // backPack.display();
 // console.log(backPack.laptop.screenSize);
-console.log(backPack.color);
+// console.log(backPack.color);
 
 /* we use this keyword inside an object to:
 Reference the current object's properties and methods
