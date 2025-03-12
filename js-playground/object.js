ec70dc57559b5bbb86a5fdb74b0a96fdad4c1e6c
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-12 00:07:18 -0500

    Remove some text inside the object file in js fundamental

--- js-playground/object.js
+++ js-playground/object.js
@@ -8,10 +8,27 @@ const myInfo = {
   },
 };
 
-let demo = document.getElementById("demo");
-demo.innerHTML = `
-    <p>Name: ${myInfo.name}</p>
-    <p>Age: ${myInfo.age}</p>
-    <p>Fav food: ${myInfo.favFood}</p>
-     <p>quote: ${myInfo.quote()}</p>
-`;
+// let demo = document.getElementById("demo");
+// demo.innerHTML = `
+//     <p>Name: ${myInfo.name}</p>
+//     <p>Age: ${myInfo.age}</p>
+//     <p>Fav food: ${myInfo.favFood}</p>
+//      <p>quote: ${myInfo.quote()}</p>
+// `;
+
+// const user = {
+//   name: "john doe",
+//   age: 32,
+//   height: 6.4,
+//   eyeColor: "brown",
+//   stomach: {
+//     smallIntestine: true,
+//     liver: true,
+//     numOfCell: "billions",
+//   },
+//   movement: function () {
+//     return "I can walk";
+//   },
+// };
+
+// console.log(user.name);
