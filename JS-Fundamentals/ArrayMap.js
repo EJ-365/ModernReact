b31ee6228e57af452ee2a9c71a9cce568f006236
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-13 14:13:59 -0500

    feat: add ArrayMap.js and index.html files

---
+++ JS-Fundamentals/ArrayMap.js
@@ -0,0 +1,17 @@
+/* The map() method creates a new array with the results of calling
+ a provided function on every element in the array. It transforms each
+  element and returns a new array of the same length.*/
+
+let fruits = ["mango", "apple", "pawpaw", "orange"];
+// let fruit = fruits.map(function (x) {
+//   return x.length;
+// });
+
+// let fruit = fruits.map((x) => x.toUpperCase());
+// console.log(fruit);
+
+// fruits.forEach((item) => console.log(item));
+
+const food = ["fufu", "rice", "eba", "noodle"];
+let result = food.splice(0, 1, "beans");
+console.log(food);
