10eae1d0e1f08064f0f569c51832c92983a3a939
Ejay Gabriel <eebudonihian@gmail.com>
2025-03-13 11:54:53 -0500

    file update

--- js-playground/object.js
+++ js-playground/object.js
@@ -52,9 +52,48 @@ function UserInfo(name, age, occupation) {
 const user1 = new UserInfo("EJ Gabriel", 22, "Student");
 const user2 = new UserInfo("Sam Tommy", 22, "Student");
 
-let demo = document.querySelector("#demo");
-demo.innerHTML = `
-<p> Name: ${user1.name} </p>
-<p> Age: ${user1.age} </p>
-<p> Occupation: ${user1.occupation} </p>
-`;
+// let demo = document.querySelector("#demo");
+// demo.innerHTML = `
+// <p> Name: ${user1.name} </p>
+// <p> Age: ${user1.age} </p>
+// <p> Occupation: ${user1.occupation} </p>
+// `;
+
+// Array destructuring: destructuring unpack object and array into variables
+let names = ["john", "smith"];
+let [firstName, lastName] = names;
+// console.log(firstName);
+
+const person = {
+  fname: "John",
+  lname: "Doe",
+  age: 35,
+};
+
+// destructuring it.
+let { fname, lname, country = "US" } = person;
+console.log(lname);
+
+// arrow function: allows us to write shorter function syntax
+// function sayHello(){
+//   return "hello"
+// }
+
+// with arrow function
+// sayHello = () => {
+//   return "hello";
+// };
+/* more concise: you don't need the square bracket for 1 or no parameter and you don't
+ need a return keyword */
+//  sayHello = () => "hello "
+
+// with parameter
+// function add(a, b) {
+//   return a + b;
+// }
+
+// with arrow function
+let add = (a, b) => a + b; // you don't have to use the return keyword or a square bracket if you have one or 2 param
+// console.log(add(3, 2));
+
+// tbd
