fa75e471ffc038f39112c04e4616eb4028f48f24
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-14 00:00:40 -0500

    Fix: Correct typo and update indexOf parameters in strings.js
    This commit fixes a typo in the `myName` variable within the `JS-Fundamentals/strings.js` file, changing "Ejay is" to "Ejayz is". Additionally, the second parameter in the `indexOf` method was changed from '0' to 1. This correction ensures the code functions as intended and improves readability.

--- JS-Fundamentals/strings.js
+++ JS-Fundamentals/strings.js
@@ -1,5 +1,5 @@
 /* All about strings in Javascript */
 
-const myName = "Ejay is a Software engineer"
-const value = myName.indexOf('a','0');
+const myName = "Ejayz is a Software engineer"
+const value = myName.indexOf('a', 1);
 console.log(value);
