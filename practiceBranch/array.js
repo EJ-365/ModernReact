f71c9526c957ac29c264007539462cedfeeeb946
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-15 09:42:19 -0500

    Feat: Introduce array.js with loop examples and reference to array methods
    This commit introduces a new file, `array.js`, which includes:
    
    - An array `num` initialized with values [2, 3, 4, 5, 5].
    - A comment referencing `allArrayMethod.md` as a cheatsheet for array methods.
    - Examples of `for` and `while` loops, including a factorial calculation using a `for` loop.

---
+++ practiceBranch/array.js
@@ -0,0 +1,26 @@
+const num = [2,3,4,5,5]
+ /*  i didn't write any code for this array because i have
+ a cheatsheet called allArrayMethod.md from there you
+ can review it */
+
+
+/* loop review */
+// for (let i = 1; i <= 5; i++){
+//     // console.log(i);
+// }
+
+// factorial
+// let factorial = 1;
+// for (let j = 5; j >= 1; j--) {
+//  factorial *= j;
+// }
+// console.log(factorial);
+
+// while loop;
+// This code will print numbers from 1-5 using while loop
+
+// let num = 0;
+// while (num < 5) {
+//     num++;
+//     console.log(num);
+// }
