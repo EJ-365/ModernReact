10b59214d8a0ee26c4c30cc30f75fa172d2f2c24
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-16 00:47:02 -0500

    Feat: Introduce decisionMaking.js and update package dependencies
    This commit introduces a new file `decisionMaking.js` to the practice branch, which includes a simple if-else conditional statement.
    
    Additionally, this commit updates `package.json` and `package-lock.json` to include `my-ai-project` as a dependency, referencing it as a file dependency. This change likely sets up local linking or dependency management for the project.

---
+++ practiceBranch/decisionMaking.js
@@ -0,0 +1,11 @@
+let message = "Good evening";
+if (message === "Good evening") {
+    console.log("Go and sleep");
+}
+
+else if (message === "Good afternoon"){
+    console.log("Good afternoon hru");
+}
+else {
+    console.log("I'm ok");
+}
