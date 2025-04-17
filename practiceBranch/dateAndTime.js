1e7366ec7183b48e0f5c86cedefef3258c784503
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-17 00:33:18 -0500

    Refactor: Update decisionMaking.js with ternary operator, switch statement, and equality checks
    This commit refactors the `decisionMaking.js` file to include examples of:
    
    - Ternary operator usage as a shorthand for if-else statements.
    - Switch statement implementation for handling multiple cases based on a date.
    - Demonstrates the usage of `!=` operator and `else` block.
    - Adds comments explaining the difference between `==` and `===` equality operators.

---
+++ practiceBranch/dateAndTime.js
@@ -0,0 +1,47 @@
+/* date and time in javascript */
+// date object in js
+// const date = new Date();
+// console.log(date);
+
+let birthDay = new Date('December 9 2002');
+// console.log(birthDay.toDateString());
+// console.log(birthDay.getDate());
+
+// date method
+
+// get the year value
+const today = new Date();
+let year = today.getFullYear();
+console.log('Year:' + year); // 2025
+
+// get the month value
+let month = today.getMonth();
+console.log('Month:' + month); // 3; because month starts from 0 - 11
+
+// get the date 
+let date = today.getDate();
+console.log('Date:' + date); // 17
+
+// get the day
+let day = today.getDay();
+console.log('day:' + day); // 4; return day in months
+
+// get hours
+let hours = today.getHours();
+console.log('Hours: ' + hours);
+
+// get minutes
+let minutes = today.getMinutes();
+console.log('Minutes: ' + minutes);
+
+// get seconds
+let seconds = today.getSeconds();
+console.log('Seconds: ' + seconds);
+
+// get milliseconds
+let milliseconds = today.getMilliseconds();
+console.log('Milliseconds: ' + milliseconds);
+
+// get time
+let time = today.getTime();
+console.log('Time:' + time);
--- practiceBranch/decisionMaking.js
+++ practiceBranch/decisionMaking.js
@@ -1,11 +1,69 @@
-let message = "Good evening";
-if (message === "Good evening") {
-    console.log("Go and sleep");
-}
+// let message = "Good evening";
+// if (message === "Good evening") {
+//     console.log("Go and sleep");
+// }
 
-else if (message === "Good afternoon"){
-    console.log("Good afternoon hru");
+// else if (message === "Good afternoon"){
+//     console.log("Good afternoon hru");
+// }
+// else {
+//     console.log("I'm ok");
+// }
+
+/* using a ternary operator: a shortcut for if-else statement 
+syntax is: conditions ? console.log("do something") : console.log("do something");*/
+
+// let userName = "Ejay";
+// userName === 'Ejeme' ? console.log(`Hey ${userName}`) : console.log(`Hi ${userName}.`);
+
+// switch statement
+// let todaysDate = 'Wednesday';
+
+// switch(todaysDate){
+
+//     case 'Monday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+    
+//     case 'Tuesday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+
+//     case 'Wednesday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+
+//     case 'Thursday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+
+//     case 'Friday': 
+//     console.log(`It's ${todaysDate}`);
+//     console.log("I'm going to houston");
+//     break;
+
+//     case 'Saturday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+
+//     case 'Sunday': 
+//     console.log(`It's ${todaysDate}`);
+//     break;
+//     default:
+//     console.log("404 error, try again");
+// }
+
+
+let user = 'John Doe';
+if (user != 'John Doe') {
+    console.log('Your name is not John Doe ');
 }
 else {
-    console.log("I'm ok");
+    console.log("Hello " + user);
 }
+
+
+/*  == (check for value only) equal operator
+   === (check for value and type => like strings, numbers etc ) strict 
+   equal operator 
+*/
