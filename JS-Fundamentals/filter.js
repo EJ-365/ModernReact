4ff10cf203969cb4217d673b0448cdc8a642081c
Ejay Gabriel <eebudonihian@gmail.com>
2025-04-24 20:18:19 -0500

    I updated filter file

--- JS-Fundamentals/filter.js
+++ JS-Fundamentals/filter.js
@@ -3,5 +3,5 @@ the test */
 
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const result = nums.filter((x) => x % 2 === 0); // returns numbers divisible by 2
-console.log(nums);
-// console.log(result);
+// console.log(nums);
+console.log(result);
