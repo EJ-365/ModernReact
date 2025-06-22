8ad213f540da068dc6fd7dbc872af03a4a23ca40
Ejay-DSKTP <eebudonihian@gmail.com>
2025-06-22 11:54:18 -0500

    Feat: Add person.js file with object literal implementation

---
+++ OOP-JS/person.js
@@ -0,0 +1,12 @@
+// Object Literals
+const person = {
+    firstName: 'Mike',
+    lastName: 'Wills',
+    age: 30,
+    greet: function(){
+        return "Good Morning";
+    },
+}
+
+console.log(person.firstName);
+console.log(person.greet());
