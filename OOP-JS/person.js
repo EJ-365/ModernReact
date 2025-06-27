32271aa5911e8f5c0329a6a7fac0c74636e0c33d
Ejay Gabriel <eebudonihian@gmail.com>
2025-06-26 22:51:15 -0500

    added this keyword and constructor

--- OOP-JS/person.js
+++ OOP-JS/person.js
@@ -1,12 +1,32 @@
 // Object Literals
+// This keyword
+/*  object constructor: constuctor allows you to create multiple instances of an object 
+instead of creating them one by one like the traditional way */
 const person = {
     firstName: 'Mike',
     lastName: 'Wills',
     age: 30,
     greet: function(){
-        return "Good Morning";
+        // return "Good Morning";
+       return `Hello Everyone my name is ${this.firstName} ${this.lastName}`
     },
 }
+// console.log(person.greet());
 
-console.log(person.firstName);
-console.log(person.greet()); 
+
+
+// object constructor: 'Remeber to  capitalize the first letter 
+function Person(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.greet = function(){
+        return `Hello my name is: ${this.firstName} ${this.lastName}`
+    }
+}
+
+// creating an instane
+const ejay = new Person('Ejay', 'Gabriel', 22);
+const recovery = new Person('Recovery', 'Martin', 24);
+console.log(ejay);
+console.log(recovery);
