9ea0bb3a13a8d86265042b5892a879b424b79b91
Ejay Gabriel <eebudonihian@gmail.com>
2025-07-01 01:30:08 -0500

    Feat: Add Person class implementation with constructor and greet method

---
+++ OOP-JS/classes.js
@@ -0,0 +1,22 @@
+/* what is classes? 
+Classes in JavaScript are templates for creating objects
+ and encapsulating data and behavior together. 
+ They provide a way to define object blueprints with properties and methods.
+*/
+
+// ES6 Classes
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    greet(){
+        return `Hello, my name is ${this.firstName} ${this.lastName}`;
+    }
+}
+
+const p1 = new Person('Ejay', 'Gabriel', 22);
+const p2 = new Person('Bri', 'Ejay', 18);
+console.log(p2.greet());
