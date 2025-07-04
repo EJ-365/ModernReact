ffe1804724262e9685d273302adae8e20eead15b
Ejay Gabriel <eebudonihian@gmail.com>
2025-07-04 02:49:04 -0500

    Feat: Implement Person and User classes with constructor and greet method

---
+++ OOP-JS/person2.js
@@ -0,0 +1,30 @@
+// ES6 classes
+/* basically everything here is ES6 even the super() method is also es6 the previous one in 
+inheritance is es5 */
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    greet(){
+        return `Hello, my name is ${this.firstName} ${this.lastName}`;
+    }
+
+}
+
+// creating another class; basically Subclass
+
+class User extends Person{
+    constructor(firstName,lastName, userName, password){
+        super(firstName, lastName) // inheriting the Person class constructor parameters
+        this.userName = userName;
+        this.password = password;
+
+    }
+}
+
+const user1 = new User('John', 'Doe', 'mrJohn', 'john123' );
+console.log(user1);
+console.log(user1.greet());
