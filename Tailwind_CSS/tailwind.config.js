067b48db9228bdf324c1c60b125cc8a765be8edc
Ejay Gabriel <eebudonihian@gmail.com>
2025-07-13 02:57:59 -0500

    created a navbar with tailwindcss and  with some javascript functionality  to make it responsive on smaller screen

--- Tailwind_CSS/tailwind.config.js
+++ Tailwind_CSS/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-color": "#f6091b",
+      }
+    },
   },
   plugins: [],
 }
