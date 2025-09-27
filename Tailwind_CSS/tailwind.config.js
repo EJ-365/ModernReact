527b5bd02b4a9bf01291a941af2ee11fc944f750
Ejay Gabriel <eebudonihian@gmail.com>
2025-09-26 23:35:23 -0500

    Create simple orange product card with Tailwind CSS

--- Tailwind_CSS/tailwind.config.js
+++ Tailwind_CSS/tailwind.config.js
@@ -1,15 +1,15 @@
 export default {
-  content: ["../.{html,js}", "../**/.{html,js}"],
+  content: ["../*.{html,js}", "../**/*.{html,js}"],
   safelist: [
-  'bg-main-color',
-  'text-main-color',
-  'border-main-color'
+    'bg-main-color',
+    'text-main-color',
+    'border-main-color'
   ],
   theme: {
-  extend: {
-  colors: {
-  'main-color': '#F60916'
-  }
+    extend: {
+      colors: {
+        'main-color': '#f7931e'  // Orange
+      }
+    },
   },
-  },
-  }
+}
