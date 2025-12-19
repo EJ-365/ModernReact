0dbc113774dfea356b4da88cfe0f09749e63e99c
Ejay-DSKTP <eebudonihian@gmail.com>
2025-12-19 01:51:22 -0600

    set up the Chefclaude projects and add any necessary things needed

---
+++ ChefClaude/src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+export default function App(){
+return (
+<div>
+<Header/>
+</div>
+)
+
+}
+
+
+function Header(){
+  return (
+    <header>
+      <h3 className="text-center text-3xl font-semibold my-10 capitalize">Chef claude</h3>
+    </header>
+  )
+}
