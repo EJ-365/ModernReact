4ceaeeca72a6c64a4c26ed5311daf6fe9f341269
Ejay-DSKTP <eebudonihian@gmail.com>
2025-12-19 18:28:07 -0600

    finished the form component ui

--- ChefClaude/src/App.jsx
+++ ChefClaude/src/App.jsx
@@ -1,18 +1,41 @@
 import "./index.css";
-export default function App(){
-return (
-<div>
-<Header/>
-</div>
-)
-
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Form />
+    </div>
+  );
 }
 
-
-function Header(){
+// header componet
+function Header() {
   return (
-    <header>
-      <h3 className="text-center text-3xl font-semibold my-10 capitalize">Chef claude</h3>
+    <header className="bg-slate-200 p-4 shadow-md">
+      <h1 className="text-center md:text-4xl text-2xl md:font-semibold capitalize md:my-4 my-2">
+        {" "}
+        <i className="bx bx-chef-hat mx-3"></i>Chef Claude
+      </h1>
     </header>
-  )
+  );
+}
+
+// form component
+function Form() { 
+  return (
+    <main>
+      <form className="flex md:flex-row flex-col justify-center md:my-16 my-20">
+        {/* input form div */}
+
+        <div className="md:mx-10 mx-auto md:my-0 my-10  ">
+          <input type="text" aria-label="Add ingredient" placeholder="e.g. oregano" className="md:w-90 w-full px-2 py-2 outline-1 rounded-sm active:ring-2 active:ring-blue-800 hover:outline-2"/>
+        </div>
+
+        {/* add ingredient button div */}
+        <div className="text-white text-center ">
+          <button className="bg-black py-2 px-4 rounded-md cursor-pointer"> <i className="bx bx-plus mx-1"></i> Add ingredient</button>
+        </div>
+      </form>
+    </main>
+  );
 }
