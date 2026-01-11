4accc08a00f26f34c534944b0a8e1f8c5c004e7b
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-10 19:15:45 -0600

    add Body component to AdviceGenerator

---
+++ AdviceGenerator/src/Body.jsx
@@ -0,0 +1,12 @@
+export default function Body() {
+    return (
+        <body className="bg-linear-to-t from-purple-200 to-purple-300 h-screen w-full">
+            <div className="flex flex-col items-center justify-center">
+
+                <div>Hello</div>
+                <div>hi</div>
+                <div>hi</div>
+            </div>
+        </body>
+    )
+}
