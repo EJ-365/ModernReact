d13256349a1b4ab28e102ebf3b685c0d6e018494
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-12 17:40:51 -0600

    Add VSCode settings for auto-save, tab size, word wrap, and custom browser; enhance Header component with accessibility title for theme toggle

--- AdviceGenerator/src/Header.jsx
+++ AdviceGenerator/src/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({toggle, setToggle}){
 
                  
                  <div>
-                    <span className="font-semibold text-2xl"><i className={`w-6 h-6 shadow-xl/30 bg-purple-100 rounded-full inset-shadow-sm bx bx-moon ${toggle && "bx bx-sun"} text-center cursor-pointer ${toggle && "text-purple-500"}`}  onClick={() => setToggle(prev => !prev)}></i></span>
+                    <span title={`${toggle ? "toggle light mode": "toggle dark mode"} `} className="font-semibold text-2xl "><i className={`w-6 h-6 shadow-xl/30 bg-purple-100 rounded-full inset-shadow-sm bx bx-moon ${toggle && "bx bx-sun"} text-center cursor-pointer ${toggle && "text-purple-500"}`}  onClick={() => setToggle(prev => !prev)}></i></span>
                 </div>
             </nav>
         </header>
