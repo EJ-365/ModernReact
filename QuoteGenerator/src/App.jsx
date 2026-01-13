3adf42fafdd26af770e10e3b0b590e5d0b46ddd7
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-13 14:38:55 -0600

    Refactor App component to include Header component; create new Header component with basic structure.

--- QuoteGenerator/src/App.jsx
+++ QuoteGenerator/src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./Header";
 function App() {
   return (
-    <h1 className="text-red-500"> Hello user </h1>
+    <>
+      <Header />
+    </>
   )
 }
 
-export default App;
+export default App; 
