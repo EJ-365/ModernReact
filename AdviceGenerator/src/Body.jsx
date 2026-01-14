c8152fa80c3b05523b2d92b5e029330c1b401db5
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-13 19:28:03 -0600

    Refactor Body component to improve advice fetching and display; update advice state management and change button action to handleAdvice function.

--- AdviceGenerator/src/Body.jsx
+++ AdviceGenerator/src/Body.jsx
@@ -1,20 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
-export default function Body({toggle}) {
+export default function Body({ toggle }) {
 
   const [advice, setAdvice] = useState({
-    id: 150,
-    text: "Life is like a water, it flows around.",
+    id: 9,
+    text: "Do what makes you happy.",
   });
+
+  const [allAdvice, setAllAdvice] = useState({});
+
+  const handleAdvice = useCallback(() => {
+    setAdvice(prevAdvice => ({
+      ...prevAdvice, id: allAdvice.id, text: allAdvice.advice
+    }))
+  }, [allAdvice]);
+
 
   async function fetchAdvice() {
     try {
       const response = await fetch("https://api.adviceslip.com/advice");
       const data = await response.json();
-      setAdvice({
-        id: data.slip.id,
-        text: data.slip.advice,
-      });
+      setAllAdvice(data.slip);
     } catch (error) {
       console.log("Error fetching data:", error);
     }
@@ -25,14 +31,14 @@ export default function Body({toggle}) {
     (async () => {
       await fetchAdvice();
     })();
-  }, []); // Runs once on mount
+  }, [handleAdvice]); // Runs once on mount
 
   return (
-    <body className={` ${toggle ? "bg-slate-950 text-white" : "bg-linear-to-b from-purple-100 to-purple-100 text-black"}`}>
+    <main className={` ${toggle ? "bg-slate-950 text-white" : "bg-linear-to-b from-purple-100 to-purple-100 text-black"}`}>
 
       <div className="flex flex-col items-center justify-center h-screen w-full">
         <div className={`rounded-3xl p-10 ${toggle ? "bg-purple-300 text-white shadow-2xs" : "bg-white shadow-2xl"} inset-shadow-sm w-96 h-auto shadow-purple-300 ${toggle && "bg-black"}`}>
-          
+
           <h3 className="text-center text-md text-purple-400 font-semibold">
             Advice #{advice.id}
           </h3>
@@ -50,7 +56,7 @@ export default function Body({toggle}) {
           <div className="flex items-center justify-center">
             <button
               className="bg-purple-700 text-white px-3 py-2 rounded-full text-center mr-4 w-1/2 text-md font-semibold align-middle cursor-pointer hover:bg-purple-800 shadow-xl shadow-purple-200 capitalize"
-              onClick={fetchAdvice}
+              onClick={handleAdvice}
             >
               {" "}
               <i className="bx bx-shuffle text-md mx-2 align-middle"></i>
@@ -60,11 +66,11 @@ export default function Body({toggle}) {
         </div>
         <span className="text-center text-sm mt-8 mb-4 text-purple-400 font-semibold">Powered by Advice Slip Api</span>
         <div className="text-purple-400 font-semibold flex items-center justify-between cursor-pointer">
-            <i className="bx bx-share text-sm mx-3"></i>
-            <i className="bx bx-bookmark text-sm"></i>
-            <i className="bx bx-copy text-sm mx-3"></i>
+          <i className="bx bx-share text-sm mx-3"></i>
+          <i className="bx bx-bookmark text-sm"></i>
+          <i className="bx bx-copy text-sm mx-3"></i>
         </div>
       </div>
-    </body>
+    </main>
   )
 }
