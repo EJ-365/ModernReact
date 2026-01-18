afabb356520713dba3d1b7c4ef0c59a672b25adc
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-18 11:59:43 -0600

    Add initial project setup for Tenzies game with React and Vite
    - Create .gitignore to exclude logs, node_modules, and editor files.
    - Add ESLint configuration for code quality and consistency.
    - Set up package.json with dependencies for React, Vite, and Tailwind CSS.
    - Create basic index.html and main.jsx files for application entry.
    - Implement App component with a simple greeting.
    - Add Tailwind CSS for styling and create an index.css file for imports.
    - Include README.md with project overview and setup instructions.

---
+++ Tenzies/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+ <div className="text-center h-screen w-full">
+   <h2 className="text-red-500 text-3xl font-bold "> Happy Sunday</h2>
+ </div>
+  )
+}
+
+/*********DO NOT ENTER*********🚫⛔⛔⛔********DO NOT ENTER*************/
+export default App;
