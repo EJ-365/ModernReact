1bc2698cddfbf3aa0c0f91198a1b9f412e234f56
Ejay-DSKTP <eebudonihian@gmail.com>
2026-01-19 20:13:28 -0600

    Refactor App component and add Die component for Tenzies game
    - Update App component layout to include a main section with a grid of Die components.
    - Create a new Die component to represent individual dice with styling.
    - Import index.css for styling consistency across the application.

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,10 +1,24 @@
+import Die from "./Die";
 function App() {
   return (
- <div className="text-center h-screen w-full">
-   <h2 className="text-red-500 text-3xl font-bold "> Happy Sunday</h2>
- </div>
+    <main className="h-screen w-full border-70 p-5 mx-auto box-border items-center justify-center flex rounded ">
+      <div className="text-[#F5F5F5] border-8 p-20 w-auto lg:space-x-4 grid lg:grid-cols-5 lg:gap-10 md:grid-cols-5 gap-4 space-x-2  grid-cols-2">
+        <Die dieNum={1}/>
+        <Die dieNum={2}/>
+        <Die dieNum={3}/>
+        <Die dieNum={4}/>
+        <Die dieNum={5}/>
+        <Die dieNum={6}/>
+        <Die dieNum={1}/>
+        <Die dieNum={1}/>
+        <Die dieNum={1}/>
+        <Die dieNum={1}/>
+      </div>
+    </main>
   )
 }
+
+
 
 /*********DO NOT ENTER*********🚫⛔⛔⛔********DO NOT ENTER*************/
 export default App;
