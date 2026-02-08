9ad09a964dd16ddb8240ab33b371378ec33a9eeb
Ejay Gabriel <eebudonihian@gmail.com>
2026-02-08 11:00:06 -0600

    feat: Integrate Tailwind CSS and Lucide React for enhanced styling and icons; refactor App component to use Header component and remove unused CSS file.

---
+++ RecipeFinder/src/Header.jsx
@@ -0,0 +1,8 @@
+import {Search} from "lucide-react";
+export default function Header(){
+    return (
+        <header>
+            <h1> Hello fom header component</h1>
+        </header>
+    )
+}
