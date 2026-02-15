d3474c7552303e90f308cfe0d8591d9ef7400933
Ejay Gabriel <eebudonihian@gmail.com>
2026-02-15 00:50:53 -0600

    feat: initialize FlashCard project with React, Vite, and Tailwind CSS; add ESLint configuration, basic app structure, and essential files including README and .gitignore

---
+++ FlashCard/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello from App.jsx</h1>
+      <h2>Upcoming next UI for flashcard</h2>
+      <h2>and then logic</h2>
+      <h2>Finally: Deployment</h2>
+      <p className="text-red-500">Duration: probally: less than 4 hours</p>
+    </div>
+  )
+}
+
+export default App;
