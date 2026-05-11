3c163c7f55df1ee215c5dad49162ba5e165f3b85
Ejay-DSKTP <eebudonihian@gmail.com>
2026-05-11 00:14:12 -0500

    feat: enhance App component with movie API integration
    - Import API_KEY and BASE_URL for fetching trending movies.
    - Implement useMovies custom hook to retrieve trending movie data.
    - Update useMemo to provide top five trending movies to context.
    - Refactor context providers for better organization in the App component.

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -7,6 +7,8 @@ import trendingMovies from "./data/trendingMovies"; // actual movies data
 import { TrendingMoviesContext } from "./Contexts/TrendingMoviesContext";
 import { PopularMoviesContext } from "./Contexts/PopularMoviesContext";
 import popularMovies from "./data/popularMovies";
+import { API_KEY, BASE_URL } from "./api/tmdb";
+import { useMovies } from "./hooks/useMovies"; // custom hook for the movie api
 
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -23,29 +25,34 @@ function App() {
     localStorage.setItem("theme", darkMode ? "dark" : "light");
   }, [darkMode]);
 
-  // useMemo for popular movies 
-  const trendingMoviesValue = useMemo(() => {
-   return {trendingMovies};
-  }, [trendingMovies]
-)
+  const trendingUrl = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
+  const trendingMovies = useMovies(trendingUrl);
+  const topFiveTrending = trendingMovies.slice(0, 5);
 
-const popularMoviesValue = useMemo(() => {
-  return {popularMovies}
-}, [popularMovies])
+  // useMemo for trending movies
+  const trendingMoviesValue = useMemo(() => {
+    return { topFiveTrending };
+  }, [topFiveTrending]);
+
+  // up next: usememo for popular movies
+  const popularMoviesValue = useMemo(() => {
+    return { popularMovies };
+  }, [popularMovies]);
+
   return (
     <>
       <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
         <div className="flex flex-col md:flex-row min-h-screen dark:bg-linear-to-r from-[#0b0a13] via-[#08070f] to-[#08070f]">
           <Navbar />
           <main className="flex-1 overflow-auto min-w-0">
-           <TrendingMoviesContext.Provider value={trendingMoviesValue}>
-            <PopularMoviesContext value={popularMoviesValue}>
-           <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-            </Routes>
-            </PopularMoviesContext>
-           </TrendingMoviesContext.Provider>
+            <TrendingMoviesContext.Provider value={trendingMoviesValue}>
+              <PopularMoviesContext.Provider value={popularMoviesValue}>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/home" element={<Home />} />
+                </Routes>
+              </PopularMoviesContext.Provider>
+            </TrendingMoviesContext.Provider>
           </main>
         </div>
       </ThemeContext.Provider>
