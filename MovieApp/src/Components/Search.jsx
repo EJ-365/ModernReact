3bc3ae46dee54d2adaf1c7fbcaca151f101b181f
Ejay-DSKTP <eebudonihian@gmail.com>
2026-05-22 14:10:23 -0500

    refactor: enhance Search component layout and functionality
    - Update Search component to improve input field styling and responsiveness.
    - Adjust layout to include a clear button for resetting the search query.
    - Refactor search icon functionality to maintain consistency in user experience.

--- MovieApp/src/Components/Search.jsx
+++ MovieApp/src/Components/Search.jsx
@@ -13,27 +13,28 @@ function Search({ setSearchQuery, setPage }) {
     }
   }
   return (
-    <section className="flex items-center justify-center">
-      <input
-        value={localQuery}
-        onKeyDown={handleKeyDown}
-        onChange={(e) => setLocalQuery(e.target.value)}
-        className="relative w-3/4 mx-5 p-1 rounded-sm border-zinc-500/ ring-2 dark:outline-none active:ring-2 bg-gray-400/20 ring-violet-800/20 dark:ring-zinc-300/20"
-        placeholder=" Search movies..."
-      />
+    <section className="flex items-center justify-center px-3">
+      <div className="relative w-full md:w-3/4 mx-2 md:mx-5">
+        <input
+          value={localQuery}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setLocalQuery(e.target.value)}
+          className="w-full p-1 pr-9 rounded-sm border-zinc-500/ ring-2 dark:outline-none active:ring-2 bg-gray-400/20 ring-violet-800/20 dark:ring-zinc-300/20"
+          placeholder=" Search movies..."
+        />
+        <i
+          onClick={() => {
+            setSearchQuery("");
+            setLocalQuery("");
+
+            setPage(1);
+          }}
+          className="bx bx-x absolute right-2 top-1/2 -translate-y-1/2 text-sm md:text-sm bg-zinc-400/30 p-1 rounded-full text-zinc-200 cursor-pointer"
+        />
+      </div>
       <i
         onClick={handleSearch}
-        className="bx bx-search-big text-2xl bg-violet-700 p-2 rounded-sm text-zinc-200 cursor-pointer"
-      />
-
-      <i
-        onClick={() => {
-          setSearchQuery("");
-          setLocalQuery("");
-     
-          setPage(1);
-        }}
-        className="bx bx-x text-sm bg-zinc-400/30 p-1 rounded-full text-zinc-200 cursor-pointer md:absolute md:right-60"
+        className="bx bx-search-big text-2xl bg-violet-700 md:p-2 p-1 rounded-sm text-zinc-200 cursor-pointer"
       />
     </section>
   );
