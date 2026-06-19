2d66b9a046aa47321bdff4138ec3198e50c162fc
Ejay Gabriel <eebudonihian@gmail.com>
2026-06-19 11:51:04 -0500

    feat: enhance Navbar and index.html with Boxicons integration
    - Add Boxicons CSS links to index.html for icon support.
    - Update Navbar component to include a Home link with an icon for improved navigation.

--- FIFAWC26/src/component/Navbar.jsx
+++ FIFAWC26/src/component/Navbar.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <header>
       <nav>
         {/* logo section */}
         <div>
-            <h2 className="text-3xl">FifaWorldCup26</h2>
+          <h2 className="text-2xl">FifaWorldCup26</h2>
         </div>
 
         {/* links section */}
         <ul>
-
+          <li>
+            <Link to="/home"><i className="bx bx-home align-middle" /> Home </Link>
+          </li>
         </ul>
       </nav>
     </header>
